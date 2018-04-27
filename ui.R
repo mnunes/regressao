431135e33ab29994ee16167d8fb3cec7ac733ced
Marcus Nunes <marcus.nunes@gmail.com>
2018-04-27 10:12:57 -0300

    Separação em painéis
    Resultados da regressão e gráficos de diagnóstico

--- ui.R
+++ ui.R
@@ -1,15 +1,37 @@
 fluidPage(
-  fluidRow(
-    column(width = 6,
-           plotOutput("plot1", height = 400, width=600,
-                      click = "plot1_click",
-                      brush = brushOpts(
-                        id = "plot1_brush"
-                      )
-           ),
-           verbatimTextOutput("model"),
-           actionButton("exclude_reset", "Reset")
+  
+
+    
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                tabPanel("Regressão",
+                column(width = 12,
+                       plotOutput("plot1", height = 400, width=600,
+                                  click = "plot1_click",
+                                  brush = brushOpts(
+                                    id = "plot1_brush"
+                                    )
+                                  ),
+                       actionButton("exclude_reset", "Reset")
+                       ),
+                tableOutput("model")
+                ),
+                tabPanel("Regressão",
+                         column(width = 12,
+                                plotOutput("plot1", height = 400, width=600,
+                                           click = "plot1_click",
+                                           brush = brushOpts(
+                                             id = "plot1_brush"
+                                           )
+                                ),
+                                actionButton("exclude_reset", "Reset")
+                         ),
+                         tableOutput("model")
+                )
+                
     )
   )
+  
+  
 )
 
